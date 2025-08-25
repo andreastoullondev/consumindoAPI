4097bf725aef557aac0d48eb1731cf95d8179c9f
Andreas Toullon <andreasgitinit@gmail.com>
2025-08-24 23:15:15 -0300

    conexão com a api por http

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict'
+
+function extrairEstilosDiceBear(){
+    const url = 'https://www.dicebear.com/styles/'
+    const response = fetch(url)
+}
+
+function carregarEstilos(){
+    const estilos = extrairEstilosDiceBear()
+}
+
+carregarEstilos()
