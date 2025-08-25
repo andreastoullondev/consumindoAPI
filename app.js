ad35be4784aa6843abf6a68faa359ec43f67fb11
Andreas Toullon <andreasgitinit@gmail.com>
2025-08-24 23:28:22 -0300

    erro 404

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict'
 
-function extrairEstilosDiceBear(){
+async function extrairEstilosDiceBear(){
+    const urlProxy = 'GET https://api.codetabs.com/v1/proxy?quest='
     const url = 'https://www.dicebear.com/styles/'
-    const response = fetch(url)
+    const response = await fetch(`${urlProxy}${url}`)
+    const html = await response.text()
+    console.log(html);
 }
 
 function carregarEstilos(){
