a5416c0a1be16aaa21b5de66f77e12417561757b
Andreas Toullon <andreasgitinit@gmail.com>
2025-08-25 21:52:57 -0300

    em breve retorna

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-'use strict'
-
-async function extrairEstilosDiceBear(){
-    const urlProxy = 'GET https://api.codetabs.com/v1/proxy?quest='
-    const url = 'https://www.dicebear.com/styles/'
-    const response = await fetch(`${urlProxy}${url}`)
-    const html = await response.text()
-    console.log(html);
-}
-
-function carregarEstilos(){
-    const estilos = extrairEstilosDiceBear()
-}
-
-carregarEstilos()
+//Retorna em breve 
